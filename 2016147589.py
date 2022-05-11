43d9c2a2ffddf1aeeac5cf0b9f29124031d662db
unknown <jongwoop97@gmail.com>
2022-05-12 04:24:52 +0900

    updated main python file

--- 2016147589.py
+++ 2016147589.py
@@ -1,18 +1,36 @@
-#proejct 2
+#project2
 
 import cv2
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
-percentage = 0.95
+percentage = 0.80
 
 if __name__ == '__main__':
-    img = cv2.imread('./faces_training/face01.pgm', 0)
+    test_dir = './faces_training'
+    faces = {}
+    entries = os.listdir(test_dir)
+    for entry in entries:
+        faces[entry] = cv2.imread(test_dir + "/" + entry, 0)
+    
+    facematrix = []
+    for key,val in faces.items():
+        facematrix.append(val.flatten())
+    facematrix = np.array(facematrix)
 
     #perform svd
-    U, s, V = np.linalg.svd(img)
+    U, s, V = np.linalg.svd(facematrix)
 
     #compute variance
-    variance = np.round(s**2/np.sum(s**2), decimals = 3)
-    dimension = 0
+    variance = np.round(s/np.sum(s), decimals = 3)
 
-    count = 1
+    dimension = 0
+    var_count = 0
+    while(var_count < percentage):
+        var_count += variance[dimension]
+        dimension += 1
+
+    print(variance)
+    print(var_count)
+    print(dimension)
