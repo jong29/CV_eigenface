c162f9987f6467326e613e0a61cc9d8c3efbb559
unknown <jongwoop97@gmail.com>
2022-05-14 23:59:35 +0900

    attempted step 2, but not done

--- 2016147589.py
+++ 2016147589.py
@@ -14,13 +14,15 @@ os.mkdir('2016147589')
 if __name__ == '__main__':
     f = open("./2016147589/output.txt", "a")
 
+    #read images
     test_dir = './faces_training'
     faces = {}
     entries = os.listdir(test_dir)
     for entry in entries:
         faces[entry] = cv2.imread(test_dir + "/" + entry, 0)
+        shape = cv2.imread(test_dir + "/" + entries[1],0).shape
     
-
+    #STEP 1 svd to determine PC
     facematrix = []
     for key,val in faces.items():
         facematrix.append(val.flatten())
@@ -32,18 +34,22 @@ if __name__ == '__main__':
 
     #compute variance
     variance = s**2/np.sum(s**2)
-
     dimension = 0
     var_count = 0
     while(var_count < percentage):
         var_count += variance[dimension]
         dimension += 1
-
+    
     f.write("############# STEP1 #############\n")
     f.write("Input Percentage: " + str(percentage) + "\n")
     f.write("Selected Dimension: " + str(dimension) + "\n\n")
-    f.write("############# STEP2 #############\n")
 
-    
+
+    #Step 2 reconstruct
+    f.write("############# STEP2 #############\n")
+    # eigenvectors = V[0:dimension , : ].copy()
+    # eigenvalues = s[0: dimension].copy()
+    # revert = np.dot(eigenvalues, eigenvectors)
+    # print(revert)
 
     f.close()
